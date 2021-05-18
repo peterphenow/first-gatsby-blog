7f24b299729536c7fca6f1706b42376246aa8b05
Peter Phenow <peter.phenow@gmail.com>
2021-05-17 21:11:31 -0500

    created a layout component for the multiple pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi There, this is my first site created with Gatsby!</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
